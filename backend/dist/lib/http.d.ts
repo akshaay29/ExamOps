bb60cd73a18b7a498919e6b50120c34310f00523
akshaay29 <guptaakshay798@gmail.com>
2026-05-16 00:39:35 +0530

    fix: remove dist from git tracking and let Railway compile fresh

--- backend/dist/lib/http.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getSingleValue(value: string | string[] | undefined): string | undefined;
--- backend/dist/lib/http.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSingleValue = getSingleValue;
-function getSingleValue(value) {
-    if (Array.isArray(value))
-        return value[0];
-    return value;
-}
-//# sourceMappingURL=http.js.map
